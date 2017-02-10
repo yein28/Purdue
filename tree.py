513e03837e0ca3da4c1ff1437a1930fd2f607002
yein28 <gkshs1234@gmail.com>
2017-02-10 00:05:25 +0000

    출력 ok

--- tree.py
+++ tree.py
@@ -16,14 +16,16 @@ class Node():
 	def input_Node( self, Node ):
 		self.dic[Node.key] = Node
 
-	# path = array / ex)[sports, baseball, kia]
+	# path = array / ex)[sports, baseball, kt]
 	def is_Exist( self, path ):
 		if path[0] in self.dic:
-			is_Exist( self.dic[path[0]], path[1:] )
+			if path.len == 1:
+				update( self.dic[path[0]] )
+			else:
+				is_Exist( self.dic[path[0]], path[1:] )
 		else:
-			inputNode = ( path[0], 50, 0 )
+			inputNode = Node( path[0], 50, 0 )
 			self.dic[path[0]] = inputNode
-		
 
 	# update the value
 	#def update( self, Node ):	
@@ -31,22 +33,33 @@ class Node():
 	def del_Node( self, Node ):
 		del self.dic[Node.key]
 
-def printNode( Node, indent ):
-	print Node
+def printNode( Node , indent = 0):
+	print( '\t' * indent + str(Node.key) )
 	for key, value in Node.dic.items():
-		print( str(key) )
-		if type(value) is Node:
-			printNode( value , 0 ) 
+		if isinstance( value.dic, dict):
+			printNode( value, indent + 1 )
+		else:
+			return 
 
 def main():
 	root = Node( "sports", 80, 1 )
 	baseball = Node( "baseball", 50, 1 )
+	soccer = Node( "soccer", 70, 1 )
+	running = Node( "running", 30, 1 )
+	kia = Node( "kia", 90, 1 ) 
+	hw = Node( "hanhwa", 50, 1 )
 
 	root.input_Node( baseball )
-	printNode( root, 0 )
+	root.input_Node( soccer )
+	root.input_Node( running )
+	soccer.input_Node( kia )
+	baseball.input_Node( kia )
+	baseball.input_Node( hw )
+#	printNode( root )
 
-	tmp = parse( "sports-baseball-kia" )
+	tmp = parse( "sports-baseball-kt" )
 	root.is_Exist( tmp ) 
+	printNode( root )
 	
 if __name__ == '__main__':
 	main()
